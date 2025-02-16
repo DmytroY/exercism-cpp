fec6810497beec6112f674628400496f3be2b42f
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-02-16 06:27:40 +0100

    last-will

---
+++ hello-world/hello_world.cpp
@@ -0,0 +1,18 @@
+#include "hello_world.h"
+#include <iostream>
+
+using namespace std;
+
+namespace hello_world
+{
+
+    string hello()
+    {
+        return "Hello, World!";
+    }
+}
+
+// int main()
+//     {
+//         std::cout << hello_world::hello() << endl;
+//     }   
