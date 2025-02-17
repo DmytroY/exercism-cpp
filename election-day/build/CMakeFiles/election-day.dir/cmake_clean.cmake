ed8f536b03e0da87e0093fa5b909a179e3f71e47
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-02-17 08:39:18 +0100

    referenses

---
+++ election-day/build/CMakeFiles/election-day.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/election-day.dir/election_day_test.cpp.obj"
+  "CMakeFiles/election-day.dir/election_day_test.cpp.obj.d"
+  "CMakeFiles/election-day.dir/test/tests-main.cpp.obj"
+  "CMakeFiles/election-day.dir/test/tests-main.cpp.obj.d"
+  "election-day.exe"
+  "election-day.exe.manifest"
+  "election-day.pdb"
+  "libelection-day.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/election-day.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
