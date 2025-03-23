6b27114b9f943f798b4dce7f9bc52eacb18278b4
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-03-23 12:06:30 +0100

    diff_sqr

---
+++ difference-of-squares/build/CMakeFiles/difference-of-squares.dir/difference_of_squares.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/difference-of-squares.dir/difference_of_squares.cpp.obj: \
+ C:\Users\YAKOVD\Exercism\cpp\difference-of-squares\difference_of_squares.cpp \
+ C:\Users\YAKOVD\Exercism\cpp\difference-of-squares\difference_of_squares.h
