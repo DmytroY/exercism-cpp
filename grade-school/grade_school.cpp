7436f77350667e6f4444064e91b1275f6f47ae5d
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-04-15 08:11:47 +0200

    grade-school3

--- grade-school/grade_school.cpp
+++ grade-school/grade_school.cpp
@@ -6,12 +6,18 @@ namespace grade_school {
 
 // TODO: add your solution here
 
+// inplement methods of school class
 map_roster school::roster() const { return _roster; }
 
 void school::add(std::string name, int grade) {
-    if(school::_roster.find(grade) != school::_roster.end()) {
-        school::_roster[grade].push_back(name);
+    auto it = school::_roster.find(grade);
+    if(it != school::_roster.end()) {
+        it->second.push_back(name);
         sort(school::_roster[grade].begin(), school::_roster[grade].end());
+
+    // if(school::_roster.find(grade) != school::_roster.end()) {
+    //     school::_roster[grade].push_back(name);
+        
     } else {
         school::_roster[grade] = {name,};
     }
