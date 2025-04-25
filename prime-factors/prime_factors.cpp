5cfaa4f708f7a585ec27bead3f6a15dae496a30e
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-04-25 20:25:33 +0200

    prime_factors2

--- prime-factors/prime_factors.cpp
+++ prime-factors/prime_factors.cpp
@@ -1,39 +1,23 @@
 #include "prime_factors.h"
 #include <iostream>
-#include <cmath>
 
 namespace prime_factors {
 
-  bool isprime(long long candidate) {
-    if(candidate < 2) return false;
-    if(candidate == 2) return true;
-    bool isprime{true};
-    for(long long d = 3; d <= std::sqrt(candidate); d += 2){
-      if(candidate % d == 0) isprime = false;
-    }
-    return isprime;
-  
-  }
-  long long next(long long prime) {
-    if(prime == 2) return 3;
-    do {
-      prime += 2;
-    } while(!isprime(prime));
-    
-    return prime;
+  long long next(long long d) {
+    if(d == 2) return 3;
+    return d + 2;
   }
   
   std::vector<long long> of(long long n){
-  
     std::vector<long  long> result{};
-    long long prime{2};
+    long long divisor{2};
   
     while(n > 1){
-      if(n % prime == 0) {
-        result.push_back(prime);
-        n /= prime;
+      if(n % divisor == 0) {
+        result.push_back(divisor);
+        n /= divisor;
       } else {
-        prime = next(prime);
+        divisor = next(divisor);
       }
     }
   
